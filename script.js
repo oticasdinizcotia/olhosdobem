d718c8af593a83c332780602843c4fbbff466b71
oticasdinizcotia <dinizlojacotia@gmail.com>
2023-12-19 15:36:26 -0300

    testes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-
-/*let wave1 = document.getElementById('wave1');
+let wave1 = document.getElementById('wave1');
 let wave2 = document.getElementById('wave2');
 let wave3 = document.getElementById('wave3');
 let wave4 = document.getElementById('wave4');
+
 
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
@@ -10,6 +10,4 @@ window.addEventListener('scroll', function(){
     wave2.style.backgroundPositionX = 40 + value * -2 +  'px';
     wave3.style.backgroundPositionX = 30 + value * 1 +  'px';
     wave4.style.backgroundPositionX = 20 + value * -1 +  'px';
-})
-
-*/ 
+});
